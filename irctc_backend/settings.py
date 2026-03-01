2ee04ae045f4c466c43d1f7763c6fff0c8290c8e
dev-code0101 <dev.codeone@proton.me>
2026-03-01 13:31:14 +0530

    dev: update ALLOWED_HOSTS

--- irctc_backend/settings.py
+++ irctc_backend/settings.py
@@ -14,7 +14,9 @@ SECRET_KEY = os.getenv("SECRET_KEY", "insecure-secret-key-change-me")
 
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS", "localhost,127.0.0.1,mysticomotive.onrender.com"
+).split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
